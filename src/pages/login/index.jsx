061ab4e17621cfd108a53dd800dc08e7a2e4cb46
Alexander Smith <hongzeyuan0@gmail.com>
2024-07-10 21:56:43 +0800

    调整登录页面的校验方式以及获取表单值的方式

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,121 +1,42 @@
 import "./index.scss";
-import { Card, Form, Input, Button } from "antd";
+import { Card, Form, Input, Button, message } from "antd";
 import logo from "@/assets/images/logo.png";
-import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { fetchLogin } from "@/store/modules/user";
 
+//phone:13800000002  code:246810
+
 const Login = () => {
   const dispatch = useDispatch();
-  const validatePhoneNumber = (value) => {
-    if (!value) {
-      return {
-        validateStatus: "error",
-        help: "手机号不能为空",
-      };
-    }
-    const phoneRegex = /^1[3-9]\d{9}$/;
-    if (!phoneRegex.test(value)) {
-      return {
-        validateStatus: "error",
-        help: "请输入有效的手机号",
-      };
-    }
-    return {
-      validateStatus: "success",
-      help: null,
-    };
-  };
-
-  const validateCode = (value) => {
-    if (!value) {
-      return {
-        validateStatus: "error",
-        help: "验证码不能为空",
-      };
-    }
-    return {
-      validateStatus: "success",
-      help: null,
-    };
-  };
-
-  // 手机号
-  const [phoneNumber, setPhoneNumber] = useState({
-    value: "13800000002",
-  });
-  // 验证码
-  const [code, setCode] = useState({
-    value: "246810",
-  });
-
-  const handleBlur = (field, validator, setter) => {
-    setter((prevState) => ({
-      ...prevState,
-      ...validator(field.value),
-    }));
-  };
-
-  const handleSubmit = async () => {
-    // 校验
-    setPhoneNumber((preState) => ({
-      ...preState,
-      ...validatePhoneNumber(phoneNumber.value),
-    }));
-    setCode((preState) => ({
-      ...preState,
-      ...validateCode(code.value),
-    }));
-    if (
-      phoneNumber.validateStatus !== "success" ||
-      code.validateStatus !== "success"
-    ) {
-      return;
-    } else {
-      // 登录
-      dispatch(fetchLogin({ mobile: phoneNumber.value, code: code.value }));
-    }
+  const onFinish = (formValue) => {
+    dispatch(fetchLogin(formValue));
   };
   return (
     <div className="login">
       <Card className="login-container">
         <img className="login-logo" src={logo} alt="" />
-        {/* 登录表单 */}
-        <Form>
+        <Form validateTrigger={["onBlur"]} onFinish={onFinish}>
           <Form.Item
-            validateStatus={phoneNumber.validateStatus}
-            help={phoneNumber.help}
+            name="mobile"
+            rules={[
+              { required: true, message: "请输入手机号" },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: "手机号码格式不对",
+              },
+            ]}
           >
-            <Input
-              size="large"
-              placeholder="请输入手机号"
-              value={phoneNumber.value}
-              onChange={(e) =>
-                setPhoneNumber({ ...phoneNumber, value: e.target.value })
-              }
-              onBlur={() =>
-                handleBlur(phoneNumber, validatePhoneNumber, setPhoneNumber)
-              }
-            />
+            <Input size="large" placeholder="请输入手机号" />
           </Form.Item>
-          <Form.Item validateStatus={code.validateStatus} help={code.help}>
-            <Input
-              size="large"
-              placeholder="请输入验证码"
-              value={code.value}
-              onChange={(e) => setCode({ ...code, value: e.target.value })}
-              onBlur={() => handleBlur(code, validateCode, setCode)}
-              maxLength={6}
-            />
+          <Form.Item
+            name="code"
+            rules={[{ required: true, message: "请输入验证码" }]}
+          >
+            <Input size="large" placeholder="请输入验证码" maxLength={6} />
           </Form.Item>
+
           <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              size="large"
-              block
-              onClick={handleSubmit}
-            >
+            <Button type="primary" htmlType="submit" size="large" block>
               登录
             </Button>
           </Form.Item>
