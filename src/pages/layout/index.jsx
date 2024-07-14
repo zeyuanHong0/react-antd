e53a6e41fe87e657dce1a92de2d9bbd59107319f
Alexander Smith <hongzeyuan0@gmail.com>
2024-07-14 18:12:35 +0800

    请求头携带 token

--- src/pages/layout/index.jsx
+++ src/pages/layout/index.jsx
@@ -1,4 +1,10 @@
+import { http } from "@/utils";
+import { useEffect } from "react";
+
 const Layout = () => {
+  useEffect(() => {
+    http.get("user/profile");
+  }, []);
   return (
     <div>
       <h1>Layout</h1>
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getToken } from "@/utils";
 
 const http = axios.create({
   baseURL: "http://geek.itheima.net/v1_0",
@@ -8,6 +9,10 @@ const http = axios.create({
 // 添加请求拦截器
 http.interceptors.request.use(
   (config) => {
+    const token = getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   (error) => {
