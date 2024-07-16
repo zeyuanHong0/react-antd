9382b9c5bbcf05bcea665fe51428d533c3f6f989
Alexander Smith <hongzeyuan0@gmail.com>
2024-07-16 20:23:54 +0800

    route配置 layout 二级路由

--- src/pages/layout/index.jsx
+++ src/pages/layout/index.jsx
@@ -6,6 +6,7 @@ import {
   LogoutOutlined,
 } from "@ant-design/icons";
 import "./index.scss";
+import { Outlet } from "react-router-dom";
 
 const { Header, Sider } = Layout;
 
@@ -52,7 +53,7 @@ const GeekLayout = () => {
           ></Menu>
         </Sider>
         <Layout className="layout-content" style={{ padding: 20 }}>
-          内容
+          <Outlet />
         </Layout>
       </Layout>
     </Layout>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "@/pages/layout";
 import Login from "@/pages/login";
+import Home from "@/pages/home";
+import Article from "@/pages/article";
+import Publish from "@/pages/publish";
 import Authorize from "@/components/authorization";
 
 const router = createBrowserRouter([
@@ -15,6 +18,20 @@ const router = createBrowserRouter([
         <Layout />
       </Authorize>
     ),
+    children: [
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "article",
+        element: <Article />,
+      },
+      {
+        path: "publish",
+        element: <Publish />,
+      },
+    ],
   },
 ]);
 
