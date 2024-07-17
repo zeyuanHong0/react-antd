9829dc854f881245ce9d32cabf93a85b98ec1779
Alexander Smith <hongzeyuan0@gmail.com>
2024-07-17 16:48:09 +0800

    layout 展示用户信息

--- src/pages/layout/index.jsx
+++ src/pages/layout/index.jsx
@@ -7,6 +7,9 @@ import {
 } from "@ant-design/icons";
 import "./index.scss";
 import { Outlet, useNavigate, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUserInfo } from "@/store/modules/user";
+import { useEffect } from "react";
 
 const { Header, Sider } = Layout;
 
@@ -31,17 +34,24 @@ const items = [
 const GeekLayout = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
-  console.log(pathname);
+  const dispatch = useDispatch();
+
+  const { userInfo } = useSelector((state) => state.user);
+
   const onMenuClick = (route) => {
     navigate(route.key);
   };
+
+  useEffect(() => {
+    dispatch(fetchUserInfo());
+  }, [dispatch]);
 
   return (
     <Layout>
       <Header className="header">
         <div className="logo" />
         <div className="user-info">
-          <span className="user-name">哲理源</span>
+          <span className="user-name">{userInfo.name}</span>
           <span className="user-logout">
             <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消">
               <LogoutOutlined /> 退出
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -5,21 +5,32 @@ const userStore = createSlice({
   name: "user",
   initialState: {
     token: getToken(),
+    userInfo: {},
   },
   reducers: {
     setToken(state, action) {
       state.token = action.payload;
       _setToken(action.payload);
     },
+    setUserInfo(state, action) {
+      state.userInfo = action.payload;
+    },
   },
 });
 
-export const { setToken } = userStore.actions;
+export const { setToken, setUserInfo } = userStore.actions;
 
 export const fetchLogin = (loginForm) => {
   return async (dispatch) => {
     const res = await http.post("/authorizations", loginForm);
     dispatch(setToken(res.data.token));
+  };
+};
+
+export const fetchUserInfo = () => {
+  return async (dispatch) => {
+    const res = await http.get("/user/profile");
+    dispatch(setUserInfo(res.data));
   };
 };
 
