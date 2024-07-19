acc4e1de3b1c70650c342f3d4bc051a474f51df8
Alexander Smith <hongzeyuan0@gmail.com>
2024-07-19 21:18:57 +0800

    安装`echarts`，基础图表渲染

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,34 @@
+import * as echarts from "echarts";
+import { useEffect, useRef } from "react";
+
+const option = {
+  xAxis: {
+    type: "category",
+    data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+  },
+  yAxis: {
+    type: "value",
+  },
+  series: [
+    {
+      data: [120, 200, 150, 80, 70, 110, 130],
+      type: "bar",
+    },
+  ],
+};
+
 const Home = () => {
-  return <div>首页</div>;
+  const echartsRef = useRef(null);
+  useEffect(() => {
+    const chartDom = echartsRef.current;
+    const myChart = echarts.init(chartDom);
+    option && myChart.setOption(option);
+  }, []);
+  return (
+    <div>
+      <div id="main" ref={echartsRef} style={{ width: 400, height: 400 }}></div>
+    </div>
+  );
 };
 
 export default Home;
