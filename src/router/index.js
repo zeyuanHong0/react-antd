71a36fcdc2902ef0eac567caf4bfe02ac2c7bb00
Alexander Smith <hongzeyuan0@gmail.com>
2024-07-31 16:05:23 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import Layout from "@/pages/layout";
 import Login from "@/pages/login";
-import Home from "@/pages/home";
-import Article from "@/pages/article";
-import Publish from "@/pages/publish";
+// import Home from "@/pages/home";
+// import Article from "@/pages/article";
+// import Publish from "@/pages/publish";
 import Authorize from "@/components/authorization";
+
+const lazyLoad = (path) => {
+  return lazy(() => import(`@/pages/${path}`));
+};
+
+const Home = lazyLoad("home");
+const Article = lazyLoad("article");
+const Publish = lazyLoad("publish");
 
 const router = createBrowserRouter([
   {
@@ -21,15 +30,27 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home />,
+        element: (
+          <Suspense fallback={"加载中"}>
+            <Home />
+          </Suspense>
+        ),
       },
       {
         path: "article",
-        element: <Article />,
+        element: (
+          <Suspense fallback={"加载中"}>
+            <Article />
+          </Suspense>
+        ),
       },
       {
         path: "publish",
-        element: <Publish />,
+        element: (
+          <Suspense fallback={"加载中"}>
+            <Publish />
+          </Suspense>
+        ),
       },
     ],
   },
